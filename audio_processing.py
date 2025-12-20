e50f244cff3a007449129f6850046236f5b6a8d8
We10Grad <jonzeldenrust@gmail.com>
2025-12-19 22:55:24 -0600

    Create audio_processing.py

---
+++ audio_processing.py
@@ -0,0 +1,45 @@
+import boto3
+import os
+import logging
+from botocore.exceptions import ClientError
+
+
+# Initialize AWS clients
+s3 = boto3.client('s3')
+polly = boto3.client('polly')
+translate = boto3.client('translate')
+transcribe = boto3.client('transcribe')
+
+# Set environment variables
+bucket_name = os.environ['S3_BUCKET']
+region = os.environ['AWS_REGION']
+prefix = os.environ.get('PREFIX', 'beta')
+
+# Create function to upload file to S3
+def upload_file(file_name, bucket, object_name=None):
+    # Upload a file to an S3 bucket
+
+    #param file_name: File to upload
+    #param bucket: Bucket to upload to
+    #param object_name: S3 object name. If not specified then file_name is used
+    
+    #return: True if file was uploaded, else False
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    try:
+        response = s3.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+response = transcribe.start_transcription_job(
+    TranscriptionJobName='string',
+    LanguageCode='en-US',
+    MediaFormat='mp3'
+    Media=('MediaFileUri': 'string'),
